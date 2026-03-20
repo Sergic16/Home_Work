e176361124610b5ffa3608865d1a1639d3c4d0dd
Sergic16 <sergic16@yandex.ru>
2026-03-20 20:15:14 +0400

    правка

--- discount.py
+++ discount.py
@@ -3,6 +3,6 @@ discount = input("Введите размер скидки (%): ")
 
 discount_price = int(price) * (1 - (int(discount) / 100))
 
-print("Стоимость товра:", price)
+print("Стоимость товара:", price)
 print("Стоимость товара со скидкой:", discount_price)
 
