3470c68a1ff698cce4d6c855df82581ec3ffa427
ozm-gg <dmitsh7@gmail.com>
2026-03-24 22:03:19 +0400

    Strings

--- 4-expenses.py
+++ 4-expenses.py
@@ -1,12 +1,23 @@
-# Создать список из трат за неделю(7 чисел)
+# Домашнее задание Строки
+# Задача 4. Вводим сумму в виде строки, например, "100 руб 10 коп".
 
-# Посчитать сумму, среднее, минимум и максимум.
-
-# Сохранить в кортеже(минимум, максимум, сумма) и вывести его
-expenses = [100, 150, 200, 250, 300, 350, 400]
-total = sum(expenses)
-average = total / len(expenses)
-minimum = min(expenses)
-maximum = max(expenses)
-result = (minimum, maximum, total)
-print(result)
+user_input = input("Введите сумму (например, 100 руб 10 коп): ")
+# Убираем пробелы и приводим к нижнему регистру
+user_input1 = user_input.strip().lower()
+# Разделяем строку на части
+user_input_mod = user_input1.split()
+# Ищем рубли и копейки
+rubles = 0
+kopecks = 0
+# Проверяем корректность значений
+if len(user_input_mod) == 4:
+    rubles = int(user_input_mod[0])
+    kopecks = int(user_input_mod[2])
+elif len(user_input_mod) == 2:
+    if user_input_mod[1] in ['руб', 'рубль', 'рублей']:
+        rubles = int(user_input_mod[0])
+    elif user_input_mod[1] in ['коп', 'копейка', 'копеек']:
+        kopecks = int(user_input_mod[0])
+total_amount = rubles + kopecks / 100
+# Выводим результат
+print(f"{total_amount:.2f} ₽")
