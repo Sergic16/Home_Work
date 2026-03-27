e51b7e5eb1d47dccb84e19c1c28546e787b2a863
Sergei <sergic16@yandex.ru>
2026-03-27 15:28:49 +0400

    2-Funcion

--- 4-expenses.py
+++ 4-expenses.py
@@ -13,19 +13,18 @@ def add_expense(expenses: List[Union[int, str]], value: int) -> List[Union[int, 
     """Добавляет строку расхода в конец общего списка"""
     user_input = int(input("Введите расход: ").strip().lower())
     expenses.append(user_input)
-    # get_total(expenses) # Печатает все расходы с их индексами
     return expenses
 
 
-def get_total(expenses: List[int]):  # Показывает сумму расходов
-    """Показывает сумму расходов"""
+def get_total_list(expenses: List[int]):  # Показывает сумму расходов
+    """Показывает список расходов"""
     for i, list_expens in enumerate(expenses):  # Печатает все расходы с их индексами
         print(i if i != 0 else "\n", list_expens)
     # Считает сумму всех числовых элементов в списке расходов
     return 0
 
 
-def get_total_sum(expenses: List[int]):
+def get_total(expenses: List[int]):
     """Считает сумму всех числовых элементов в списке расходов, игнорируя строку заголовка"""
     return sum(summa for summa in expenses if isinstance(summa, (int, float)))
 
@@ -34,13 +33,13 @@ def delete_expence(expenses: List[int], index: int):
     """Удаляет строку расхода из списка по номеру"""
 
     # Печатает все расходы с их индексами, для удобства пользователя
-    get_total(expenses)
+    get_total_list(expenses)
     user_select_index = int(
         input("Введите номер расхода, который нужно удалить: ").strip().lower())
     if 0 < user_select_index < len(expenses):
         expenses.pop(user_select_index)
         print(
-            f"Расход удален. Сумма оставшихся расходов: {get_total_sum(expenses)}")
+            f"Расход удален. Сумма оставшихся расходов: {get_total(expenses)}")
     else:
         print("Некорректный номер расхода. Пожалуйста, попробуйте снова.")
         # Рекурсивный вызов функции для повторного запроса ввода
@@ -49,19 +48,17 @@ def delete_expence(expenses: List[int], index: int):
 
 def get_average(expenses: List[int]):  # Показывает средний расход
     """Показывает средний расход"""
-    total = get_total_sum(expenses)
+    total = get_total(expenses)
     if len(expenses) > 1:
-        print(f"Средний расход: {total / (len(expenses) - 1)}")
-    return 0
-
+        total_sr = total / (len(expenses) - 1)
+    return total_sr
 
 def print_report(expenses: List[int]):  # Печатает красивый отчет
     """Печатает красивый отчет"""
     print("Отчет по расходам:")
-    get_total(expenses)
-    print(f"Сумма: {get_total_sum(expenses)}")
-    get_average(expenses)
-    # print(f"Средний расход: {get_average(expenses)}")
+    get_total_list(expenses)
+    print(f"Сумма: {get_total(expenses)}")
+    print(f"Средний расход: {get_average(expenses)}")
 
 menus = (" ", "Добавить расход", "Показать все расходы",
          "Показать сумму", "показать средний расход", "Удалить расход по номеру", "Показать отчет", "Выход")
@@ -77,13 +74,13 @@ while user_select not in menus:
         case "1":
             add_expense(expenses, value)  # добавляет расход
         case "2":
-            get_total(expenses)  # показывает все расходы
+            get_total_list(expenses)  # показывает все расходы
         case "3":
             # get_total(expenses)  # возвращает сумму
-            print(f"Сумма всех расходов: {get_total_sum(expenses)}")
+            print(f"Сумма всех расходов: {get_total(expenses)}")
         case "4":
             get_average(expenses)  # возвращает средний расход
-            # print(get_average(expenses))
+            print(get_average(expenses))
         case "5":
             delete_expence(expenses, index)  # удалить расход
         case "6":
